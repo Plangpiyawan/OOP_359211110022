6711e30039461c9439d72fa55b84bfbd9266b936
Plangpiyawan <pypapiyawan2580@gmail.com>
2017-12-04 14:36:33 +0700

    Nestedloop while for 1

--- src/OOPlab3/TestNestedLoop.java
+++ src/OOPlab3/TestNestedLoop.java
@@ -4,12 +4,15 @@ public class TestNestedLoop {
     public static void main(String[] args) {
 
         //Nested Loop
+        int x=1;
         for (int i = 1; i <= 8; i++) {
             for (int j = 1; j <= i; j++) {
                 if (i==8 || j==i||j==1)
-                    System.out.print("*");
-                else
-                    System.out.print("#");
+                    System.out.print("*\t");
+                else {
+                    System.out.print(x + "\t");
+                    x++;
+                }
 
             } //j
             System.out.println();
