ffb8d3eb104fc415ea83b6b80dbba5f58a1825d2
Plangpiyawan <Pypiyawan2580@gmail.com>
2017-12-25 18:12:39 +0700

    mystring buffer

---
+++ src/opplab6/myStrBuffer.java
@@ -0,0 +1,25 @@
+package opplab6;
+
+public class myStrBuffer {
+    public static void main(String[] args) {
+     //  StringBuffer
+                StringBuffer buffer = new StringBuffer();
+                buffer.append("Hello");
+                buffer.append(" ");
+                buffer.append("to");
+                buffer.append(" ");
+                buffer.append("RMUTSV");
+                 buffer.append(" ");
+                buffer.append(2017);
+                System.out.println(buffer);
+                buffer.replace(0,5,"university of");
+                System.out.println(buffer);
+                System.out.println(buffer.reverse());
+                buffer.setCharAt(0,'T');
+                System.out.println(buffer);
+
+
+
+    }//main
+
+}//class
